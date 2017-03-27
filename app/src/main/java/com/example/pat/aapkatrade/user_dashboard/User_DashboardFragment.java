e06ddd77ff05646d57c086c04d9aea268384b413
nishantmalaiya <nishantmalaiya@gmail.com>
2017-03-27 18:33:22 +0530

    hello

--- app/src/main/java/com/example/pat/aapkatrade/user_dashboard/User_DashboardFragment.java
+++ app/src/main/java/com/example/pat/aapkatrade/user_dashboard/User_DashboardFragment.java
@@ -69,7 +69,6 @@ public class User_DashboardFragment extends Fragment {
 
         user_image = app_sharedpreference.getsharedpref("profile_pic","");
         Picasso.with(getActivity()).load(user_image)
-
                 .error(R.drawable.ic_profile_user)
                 .into(imageviewpp);
         textViewName = (TextView) v.findViewById(R.id.textViewName);
