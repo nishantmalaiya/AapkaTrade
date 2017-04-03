a987ced63790f1cfed1ec7ef94054d5534234251
Arvind <arvindkumaryadav937@gmail.com>
2017-04-03 10:17:14 +0530

    hi

---
+++ app/src/main/java/com/example/pat/aapkatrade/payment/PaymentActivity.java
@@ -0,0 +1,16 @@
+package com.example.pat.aapkatrade.payment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pat.aapkatrade.R;
+
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
