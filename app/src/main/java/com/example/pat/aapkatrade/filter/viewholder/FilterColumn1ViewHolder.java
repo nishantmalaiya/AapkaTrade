fc38412d9409f8f886213b69254c76332307f295
shankitkumar <sankit@aapkatrade.com>
2017-04-10 10:50:56 +0530

    Hi I have Commited

--- app/src/main/java/com/example/pat/aapkatrade/filter/viewholder/FilterColumn1ViewHolder.java
+++ app/src/main/java/com/example/pat/aapkatrade/filter/viewholder/FilterColumn1ViewHolder.java
@@ -1,8 +1,10 @@
 package com.example.pat.aapkatrade.filter.viewholder;
 
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.pat.aapkatrade.R;
@@ -14,10 +16,12 @@ import com.example.pat.aapkatrade.R;
 public class FilterColumn1ViewHolder extends RecyclerView.ViewHolder {
     public TextView filterName;
     public ImageView imageView;
+    public LinearLayout filterColumn1;
 
     public FilterColumn1ViewHolder(View itemView) {
         super(itemView);
         filterName = (TextView) itemView.findViewById(R.id.tv_filter_name);
         imageView = (ImageView) itemView.findViewById(R.id.imageView4);
+        filterColumn1 = (LinearLayout) itemView.findViewById(R.id.filter_column1);
     }
 }
